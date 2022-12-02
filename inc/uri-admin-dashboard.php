960a5c55ebe2c2acd28e1088c9fac952f0dc139a
Brandon Fuller <bjcfuller@uri.edu>
2022-12-02 13:18:42 -0500

    Remove functions included in uri-admin plugin

--- inc/uri-admin-dashboard.php
+++ inc/uri-admin-dashboard.php
@@ -12,39 +12,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 
 /**
- * Override the default "Thank you for using WordPress admin footer".
- */
-function uri_admin_footer() {
-	echo '<p class="footer-about-wp"><a href="https://www.uri.edu/wordpress/" target="_blank">Learn more about WordPress at URI</a>.</p>';
-}
-add_filter( 'admin_footer_text', 'uri_admin_footer' );
-
-
-
-/**
- * Remove unwanted WP Admin Dashboard boxes
- */
-function uri_admin_remove_boxes() {
-	// @see https://www.wpexplorer.com/customize-wordpress-admin-dashboard/
-	// limit our handed-down customizations to users who can't manage options
-	if ( ! current_user_can( 'manage_options' ) ) {
-		// these are the default WP meta boxes
-		remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
-		remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
-		remove_meta_box( 'dashboard_primary', 'dashboard', 'normal' );
-		remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
-		remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
-		remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
-		remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
-		remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
-		remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' );
-	}
-
-}
-add_action( 'admin_init', 'uri_admin_remove_boxes' );
-
-
-/**
  * Add a metabox to the dashboard.
  */
 function uri_admin_add_dashboard_widgets() {
